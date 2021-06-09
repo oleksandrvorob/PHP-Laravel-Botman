df8db714c00f9931dd520a5e88e8d7754614a1b1
ashonibarev <victorashon007@gmail.com>
2021-06-08 23:21:51 -0400

    latest code

---
+++ app/Providers/BotMan/DriverServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Providers\BotMan;
+
+use BotMan\BotMan\Drivers\DriverManager;
+use BotMan\Studio\Providers\DriverServiceProvider as ServiceProvider;
+
+class DriverServiceProvider extends ServiceProvider
+{
+    /**
+     * The drivers that should be loaded to
+     * use with BotMan.
+     *
+     * @var array
+     */
+    protected $drivers = [];
+
+    public function boot()
+    {
+        parent::boot();
+
+        foreach ($this->drivers as $driver) {
+            DriverManager::loadDriver($driver);
+        }
+    }
+}
